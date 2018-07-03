fd290b1b0bb71b777bcd6f77ddf690922f261fa5
Herwindyo Purbo Kusumo <purbo.kusumo@acommerce.asia>
2018-07-03 14:57:51 +0700

    pub sub without lib celery_pubsub

--- config/celery.py
+++ config/celery.py
@@ -1,7 +1,7 @@
 from celery import Celery
 from config.log import logger
 import celery_pubsub
-import celery as main_celery
+import celery as app
 
 
 celery = Celery(
@@ -11,10 +11,24 @@ celery = Celery(
 )
 
 
-@main_celery.task
+@app.task(
+    name="pub_sub_add"
+)
 def add(*args, **kwargs):
-    logger.info("[consumer] {}".format(args[0]))
+    logger.info("[consumer 2] {}".format(args[0]))
     return args[0]
 
 
-celery_pubsub.subscribe('some.topic', add)
+@app.task(
+    name="pub_sub_main"
+)
+def main_publisher_distributed():
+    for x in range(0, 100000):
+        # res = celery_pubsub.publish('some.topic', x)
+        res = add.delay(x)
+        logger.info("[publisher 2] {}".format(x))
+        # not working
+        # res = celery_pubsub.publish('some.topic', x)
+        # logger.info("[publisher] {}".format(res))
+
+# celery_pubsub.subscribe('some.topic', add)
