59700d71b96332c4d611029eaad120b2d17573aa
Herwindyo Purbo Kusumo <purbo.kusumo@acommerce.asia>
2018-07-07 22:45:53 +0700

    enabling pub sub for catching return val from other consumer

--- config/celery.py
+++ config/celery.py
@@ -1,7 +1,8 @@
 from celery import Celery
 from config.log import logger
 import celery as app
-
+from celery.result import allow_join_result
+import random
 
 celery = Celery(
     'smarti-consumer',
@@ -11,18 +12,27 @@ celery = Celery(
 
 
 @app.task(
-    name="pub_sub_add"
+    name="pub_sub_add_main",
+    trail=True
 )
 def add(*args, **kwargs):
-    logger.info("[consumer 2] {}".format(args[0]))
-    return args[0]
+    n_data = []
+    for n in range(10):
+        n_data.append(random.randint(1, 101))
+
+    logger.info("Loop ke : {}. {}".format(args[0], n_data))
+    return n_data
 
 
 @app.task(
-    name="pub_sub_main"
+    name="pub_sub_main",
+    trail=True
 )
 def main_publisher_distributed():
+    n_data = []
     for x in range(0, 100000):
         res = add.delay(x)
-        logger.info("[publisher 2] {}".format(x))
+        with allow_join_result():
+            n_data.append(res.get())
 
+    logger.info("Result data : {} ".format(n_data))
